1493d1199c517d26403dea37330c2ea974f941de
Kuswanth Kumar <kuswanthkumarbokam16@gmail.com>
2023-06-09 19:08:57 +0530

    first commit

---
+++ src/utils/fetchApiData.jsx
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+const BASE_URL = "https://youtube-v31.p.rapidapi.com";
+
+const options = {
+  method: "GET",
+  url: BASE_URL,
+  params: {
+    maxResults: 50,
+  },
+  headers: {
+    "X-RapidAPI-Key": "fb3b555059mshe8f5ef336dfef19p1a452bjsn628eb99ef905",
+    "X-RapidAPI-Host": "youtube-v31.p.rapidapi.com",
+  },
+};
+
+export const fetchFromAPI = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+
+  return data;
+};
